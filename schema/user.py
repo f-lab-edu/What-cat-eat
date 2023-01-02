f22e871c2ec15d9e223e780f581ef0e6178b673e
heyoni <74006293+heyoni@users.noreply.github.com>
2023-01-02 16:20:15 +0900

    Revert "Feat/#7 클린 아키텍쳐 적용"

--- schema/user.py
+++ schema/user.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel, Field
+from fastapi import HTTPException
 import re
-from typing import Optional
 
 
 class UserBase(BaseModel):
-    id: Optional[int] = Field(primary_key=True)
     nickname: str = Field(min_length=1, max_length=15)
     password: str
+    password_check: str
 
 
 class UserCreate(UserBase):
@@ -25,24 +25,20 @@ class UserGet(BaseModel):
         orm_mode = True
 
 
-class ValidatePassword(BaseModel):
-    success: bool
-    status_code: int = 200
-    error: str = None
+def validate_password(values):
+    if values.password_check != values.password:
+        raise HTTPException(status_code=400, detail="비밀번호가 일치하지 않습니다.")
 
+    if len(values.password) < 8:
+        raise HTTPException(status_code=400, detail="비밀번호 길이가 너무 짧습니다.")
 
-def validate_password(password) -> ValidatePassword:
-    if len(password) < 8:
-        return ValidatePassword(success=False, status_code=400, error="비밀번호가 너무 짧습니다.")
-
-    if re.search(r"[a-z]", password) is None:
-        return ValidatePassword(
-            success=False, status_code=400, error="비밀번호는 한개 이상의 영소문자가 필수적으로 들어가야 합니다."
+    if re.search(r"[a-z]", values.password) is None:
+        raise HTTPException(
+            status_code=400, detail="비밀번호는 한개 이상의 영소문자가 필수적으로 들어가야 합니다."
+        )
+    if re.search(r"\d", values.password) is None:
+        raise HTTPException(
+            status_code=400, detail="비밀번호는 한개 이상의 숫자가 필수적으로 들어가야 합니다."
         )
 
-    if re.search(r"\d", password) is None:
-        return ValidatePassword(
-            success=False, status_code=400, error="비밀번호는 한개 이상의 숫자가 필수적으로 들어가야 합니다."
-        )
-
-    return {"success": True}
+    return values.password
--- tests/test_user.py
+++ tests/test_user.py
@@ -14,6 +14,20 @@ def test_get_unknown_user():
     assert response.json() == {"detail": "사용자를 찾을 수 없습니다."}
 
 
+def test_dont_match_password():
+    response = client.post(
+        f"{BASE_URL}/user/",
+        json={
+            "user_id": "test_user",
+            "nickname": "test",
+            "password": "testtest1",
+            "password_check": "testtest2",
+        },
+    )
+    assert response.status_code == 400
+    assert response.json() == {"detail": "비밀번호가 일치하지 않습니다."}
+
+
 def test_short_password():
     response = client.post(
         f"{BASE_URL}/user/",
@@ -21,6 +35,7 @@ def test_short_password():
             "user_id": "test_user",
             "nickname": "test",
             "password": "1",
+            "password_check": "1",
         },
     )
     assert response.status_code == 400
@@ -34,6 +49,7 @@ def test_without_lowercases_in_password():
             "user_id": "test_user",
             "nickname": "test",
             "password": "12345678",
+            "password_check": "12345678",
         },
     )
     assert response.status_code == 400
@@ -47,6 +63,7 @@ def test_without_numbers_in_password():
             "user_id": "test_user",
             "nickname": "test_user",
             "password": "testwithoutnumber",
+            "password_check": "testwithoutnumber",
         },
     )
     assert response.status_code == 400
@@ -60,14 +77,12 @@ def test_max_length_in_userid():
             "user_id": "testuserhelloworld",
             "nickname": "test_user",
             "password": "testuser1",
+            "password_check": "testuser1",
         },
     )
     assert response.status_code == 422
     assert response.json()["detail"][0]["loc"] == ["body", "user_id"]
-    assert (
-        response.json()["detail"][0]["msg"]
-        == "ensure this value has at most 15 characters"
-    )
+    assert response.json()["detail"][0]["msg"] == "ensure this value has at most 15 characters"
 
 
 def test_max_length_in_nickname():
@@ -77,14 +92,12 @@ def test_max_length_in_nickname():
             "user_id": "test_user",
             "nickname": "testuserhelloworld",
             "password": "testuser1",
+            "password_check": "testuser1",
         },
     )
     assert response.status_code == 422
     assert response.json()["detail"][0]["loc"] == ["body", "nickname"]
-    assert (
-        response.json()["detail"][0]["msg"]
-        == "ensure this value has at most 15 characters"
-    )
+    assert response.json()["detail"][0]["msg"] == "ensure this value has at most 15 characters"
 
 
 def test_min_length_in_userid():
@@ -94,14 +107,12 @@ def test_min_length_in_userid():
             "user_id": "",
             "nickname": "test_user",
             "password": "testuser1",
+            "password_check": "testuser1",
         },
     )
     assert response.status_code == 422
     assert response.json()["detail"][0]["loc"] == ["body", "user_id"]
-    assert (
-        response.json()["detail"][0]["msg"]
-        == "ensure this value has at least 1 characters"
-    )
+    assert response.json()["detail"][0]["msg"] == "ensure this value has at least 1 characters"
 
 
 def test_min_length_in_nickname():
@@ -111,14 +122,12 @@ def test_min_length_in_nickname():
             "user_id": "test_user",
             "nickname": "",
             "password": "testuser1",
+            "password_check": "testuser1",
         },
     )
     assert response.status_code == 422
     assert response.json()["detail"][0]["loc"] == ["body", "nickname"]
-    assert (
-        response.json()["detail"][0]["msg"]
-        == "ensure this value has at least 1 characters"
-    )
+    assert response.json()["detail"][0]["msg"] == "ensure this value has at least 1 characters"
 
 
 def test_userid_is_empty():
@@ -127,6 +136,7 @@ def test_userid_is_empty():
         json={
             "nickname": "test_user",
             "password": "testuser1",
+            "password_check": "testuser1",
         },
     )
     assert response.status_code == 422
@@ -140,6 +150,7 @@ def test_nickname_is_empty():
         json={
             "user_id": "test_user",
             "password": "testuser1",
+            "password_check": "testuser1",
         },
     )
     assert response.status_code == 422
@@ -154,13 +165,14 @@ def test_create_user():
             "user_id": "test_user",
             "nickname": "test_user",
             "password": "testuser1",
+            "password_check": "testuser1",
         },
     )
     global ID
-    ID = response.json()["id"]
+    ID = response.json()['id']
 
     assert response.status_code == 201
-    assert response.json() == {"id": ID, "nickname": "test_user"}
+    assert response.json() == {'id': ID, 'nickname': 'test_user'}
 
 
 def test_create_existing_user():
@@ -170,6 +182,7 @@ def test_create_existing_user():
             "user_id": "test_user",
             "nickname": "test_user",
             "password": "testuser1",
+            "password_check": "testuser1",
         },
     )
     assert response.status_code == 409
@@ -179,7 +192,7 @@ def test_create_existing_user():
 def test_get_user():
     response = client.get(f"{BASE_URL}/user/{ID}/")
     assert response.status_code == 200
-    assert response.json() == {"id": ID, "nickname": "test_user"}
+    assert response.json() == {'id': ID, 'nickname': 'test_user'}
 
 
 def test_update_user():
@@ -188,13 +201,18 @@ def test_update_user():
         json={
             "nickname": "amend_user",
             "password": "testuser1",
+            "password_check": "testuser1",
         },
     )
     assert response.status_code == 200
-    assert response.json() == {"id": ID, "nickname": "amend_user"}
+    assert response.json() == {'id': ID, 'nickname': 'amend_user'}
 
 
 def test_delete_user():
-    response = client.delete(f"{BASE_URL}/user/{ID}/")
+    response = client.delete(
+        f"{BASE_URL}/user/{ID}/"
+    )
     assert response.status_code == 200
     assert response.json() == {"detail": "탈퇴되었습니다."}
+
+
